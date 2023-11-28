44b9243e03798362e3e2d350c096efa32e59fba2
Antonin LECOCQ <105948898+DJ4nto@users.noreply.github.com>
2023-11-28 14:22:34 +0100

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,6 +1,6 @@
 import random, string
 
-def generate_password(len, complex):
+def generate_password(len, complex): # Fonction for the paassword generation
     password = ""
     if complex == 1:
         for i in range(len):
@@ -13,7 +13,7 @@ def generate_password(len, complex):
             password += random.choice(string.ascii_letters + string.ascii_uppercase + string.digits + string.punctuation)
     return password
 
-len = int(input("Longueure du mot de passe :"))
-complex = int(input("Complexitée du mot de passe (1 -> 3) :"))
+len = int(input("Longueure du mot de passe :")) # lenght of the password (no limit)
+complex = int(input("Complexitée du mot de passe (1 -> 3) :")) # complexity of the password
 
 print(generate_password(len, complex))
